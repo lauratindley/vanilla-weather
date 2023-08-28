51284600290332f8db19be9c451b5ad80e349e38
lauratindley <142441126+lauratindley@users.noreply.github.com>
2023-08-28 10:37:30 +0100

    adding last updated time

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  console.log(date);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    " Saturday",
+  ];
+  let day = days[date.getDay()];
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${day} ${hours}:${minutes}`;
+}
+
 function displayWeather(response) {
   console.log(response);
   document.querySelector("h1").innerHTML = response.data.city;
@@ -12,6 +36,9 @@ function displayWeather(response) {
   );
   document.querySelector("#current-weather-description").innerHTML =
     response.data.daily[0].condition.description;
+  document.querySelector("#current-time").innerHTML = formatTime(
+    response.data.daily[0].time * 1000
+  );
 }
 
 let initialCity = "London";
